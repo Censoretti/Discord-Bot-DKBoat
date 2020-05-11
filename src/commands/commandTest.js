d0efc31723cfc4826d4727ef1364863059e18ec7
censoretti <rodrigopaiotti96@gmail.com>
2020-05-11 00:12:11 -0300

    command test ajust

--- src/commands/commandTest.js
+++ src/commands/commandTest.js
@@ -1,12 +1,9 @@
 module.exports = {
 	name: 'execution',
 	// role: 'manager',
-	aliases: ['exec', 'bau'],
+	aliases: ['exec', 'x'],
 	// eslint-disable-next-line no-unused-vars
 	execute: async (message, args, cooldowns, timestamps, client, admPass, managerPass) => {
-		const akuma = require('../docs/assets/628028186709458945/akumas.json')
-
-		console.log(akuma.rankSS)
-		console.log(akuma.rankSS[0]);
+		// command test
 	},
 };

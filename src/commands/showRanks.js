2c3ac9110295a8d9d7867fa6c7fa9d9499ff72e0
censoretti <rodrigopaiotti96@gmail.com>
2020-05-11 15:59:47 -0300

    better way to show ranks

--- src/commands/showRanks.js
+++ src/commands/showRanks.js
@@ -6,7 +6,7 @@ module.exports = {
 	name: 'rank',
 	description: 'Show ranks',
 	aliases: ['ranks'],
-	// onRP: off,
+	onRP: 'off',
 	// cooldown: 60,
 	// guildOnly: true,
 	// usage: '',
@@ -19,122 +19,100 @@ module.exports = {
 		if(guildConfig[guildIdBase].parentGuild.situation) {
 			guildId = guildConfig[guildIdBase].parentGuild.id
 		}
-
-		let rankOf = 'me'
-
-		let test = true
-
+		const ranks = new Discord.Collection()
+		const rankFiles = await fs.readdir(`src/docs/ranks/${guildId}`)
+			.catch(err => console.log('[#commandFiles]', err))
+		for (const file of rankFiles) {
+			const rankName = require(`../docs/ranks/${guildId}/${file}`)
+			ranks.set(rankName.config.name, rankName);
+		}
+		let rank
+		// if(admPass) return
+		// if(managerPass) return
 		if(args[0]) {
-			if(args[0] == 'invite' 
-			|| args[0] == 'invites') {
-				rankOf = 'invites'
-				test = false
-			} else if(args[0] == 'forca' 
-			|| args[0] == 'força') {
-				rankOf = 'str'
-				test = false
-			} else if(args[0] == 'resistencia' 
-			|| args[0] == 'resistência') {
-				rankOf = 'res'
-				test = false
-			} else if(args[0] == 'velocidade' 
-			|| args[0] == 'vel') {
-				rankOf = 'spd'
-				test = false
-			} else if(args[0] == 'destreza' 
-			|| args[0] == 'dex') {
-				rankOf = 'dex'
-				test = false
-			} else if(args[0] == 'level' 
-			|| args[0] == 'rp') {
-				rankOf = 'lv'
-				test = false
-			} else if(message.mentions.users.size) {
-				rankOf = 'mention'
-			} else {
-				test = true
-			}
-		}
-		
-		if(rankOf == 'me') {
-			const me = message.author.id
-			const appearance = require(`../docs/sheets/${me}`).rp.appearance
-			if(appearance == 'none') {
-				return message.channel.send('Você não tem ficha não maluco')
-			}
-			const sheetName = require(`../docs/sheets/${me}`).rp.name
+			if(message.mentions.users.size) {
+				let mention = ''
+				message.mentions.users.map(user => { mention = user.id})
+				const appearanceM = require(`../docs/sheets/${mention}`).rp.appearance
+				if(appearanceM == 'none') {
+					return message.channel.send('Esse cara ai não tem ficha não mano')
+				}
+				let descriptionReply = ''
+	
+				for(const files of rankFiles) {
+					const rankFile = require(`../docs/ranks/${guildId}/${files}`)
+					descriptionReply += `Rank de ${rankFile.config.nameDesciption}: ${rankFile.users[mention].rank}º ${rankFile.config.description}${rankFile.users[mention].value}\n`
+				}
+	
+				const sheetName = require(`../docs/sheets/${mention}`).rp.name
+	
+				const embedMention = new Discord.MessageEmbed()
+					.setColor('#00ff00')
+					.setThumbnail(appearanceM)
+					.setTitle(`Rank de ${sheetName}`)
+					.setDescription(descriptionReply)
+					.setTimestamp()
+					.setFooter('[Bot feito por Censoretti]', 'https://cdn.discordapp.com/attachments/613477001071951915/703825043066585168/Screenshot_5.png')
+	
+				return message.channel.send(embedMention)
+			} else if(args[0] == 'me'
+			|| args[0] == 'eu'
+			|| args[0] == 'meu') {
+				const me = message.author.id
+				const appearance = require(`../docs/sheets/${me}`).rp.appearance
+				if(appearance == 'none') {
+					return message.channel.send('Você não tem ficha não maluco')
+				}
+				const sheetName = require(`../docs/sheets/${me}`).rp.name
 
-			let descriptionReply = ''
+				let descriptionReply = ''
 
-			const rankFiles = await fs.readdir(`src/docs/ranks/${guildId}`)
-				.catch(err => console.log('[#RANKFILES/RANKUPDATE]', err))
+				for(const files of rankFiles) {
+					const rankFile = require(`../docs/ranks/${guildId}/${files}`)
+					descriptionReply += `Rank de ${rankFile.config.nameDesciption}: ${rankFile.users[me].rank}º ${rankFile.config.description}${rankFile.users[me].value}\n`
+				}
 
-			for(const files of rankFiles) {
-				const rankFile = require(`../docs/ranks/${guildId}/${files}`)
-				descriptionReply += `Rank de ${rankFile.config.nameDesciption}: ${rankFile.users[me].rank}º ${rankFile.config.description}${rankFile.users[me].value}\n`
-			}
+				const embedMe = new Discord.MessageEmbed()
+					.setColor('#00ff00')
+					.setThumbnail(appearance)
+					.setTitle(`Rank de ${sheetName}`)
+					.setDescription(descriptionReply)
+					.setTimestamp()
+					.setFooter('[Bot feito por Censoretti]', 'https://cdn.discordapp.com/attachments/613477001071951915/703825043066585168/Screenshot_5.png')
 
-			const embedMe = new Discord.MessageEmbed()
-				.setColor('#00ff00')
-				.setThumbnail(appearance)
-				.setTitle(`Rank de ${sheetName}`)
-				.setDescription(descriptionReply)
-				.setTimestamp()
-				.setFooter('[Bot feito por Censoretti]', 'https://cdn.discordapp.com/attachments/613477001071951915/703825043066585168/Screenshot_5.png')
-
-			return message.channel.send(embedMe)
-		}
-
-
-		if(message.mentions.users.size) {
-			let mention = ''
-			message.mentions.users.map(user => { mention = user.id})
-			const appearanceM = require(`../docs/sheets/${mention}`).rp.appearance
-			if(appearanceM == 'none') {
-				return message.channel.send('Esse cara ai não tem ficha não mano')
-			}
+				return message.channel.send(embedMe) 
 			
-			let descriptionReply = ''
-
-			const rankFiles = await fs.readdir(`src/docs/ranks/${guildId}`)
-				.catch(err => console.log('[#RANKFILES/RANKUPDATE]', err))
-
-			for(const files of rankFiles) {
-				const rankFile = require(`../docs/ranks/${guildId}/${files}`)
-				descriptionReply += `Rank de ${rankFile.config.nameDesciption}: ${rankFile.users[mention].rank}º ${rankFile.config.description}${rankFile.users[mention].value}\n`
+			} else {
+				const response = args[0].charAt(0).toUpperCase() + args[0].toLowerCase().slice(1)
+				rank = ranks.get(response)
+				|| ranks.find(cmd => cmd.config.aliases && cmd.config.aliases.includes(response))
+				if(!rank) {
+					return message.channel.send(`${response} não é um rank configurado`)
+				}
 			}
-
-			const sheetName = require(`../docs/sheets/${mention}`).rp.name
-
-			const embedMention = new Discord.MessageEmbed()
-				.setColor('#00ff00')
-				.setThumbnail(appearanceM)
-				.setTitle(`Rank de ${sheetName}`)
-				.setDescription(descriptionReply)
-				.setTimestamp()
-				.setFooter('[Bot feito por Censoretti]', 'https://cdn.discordapp.com/attachments/613477001071951915/703825043066585168/Screenshot_5.png')
-
-			return message.channel.send(embedMention)
-		}
-
-		if(test) {
-			return message.channel.send(`Nenhum rank configurado para ${args[0]}`)
-		}
-		
-		const data = require(`../docs/ranks/${guildId}/rank${rankOf}.json`)
+		} else {
+			const verifyMessage = await message.channel.send('Rank do que você quer?')
+			const responseMessage = await verifyMessage.channel.awaitMessages(msg => msg.content, { max: 1, min: 1, time: 60000 })
+			const response = responseMessage.first().content.charAt(0).toUpperCase() + responseMessage.first().content.toLowerCase().slice(1)
+			rank = ranks.get(response)
+			|| ranks.find(cmd => cmd.config.aliases && cmd.config.aliases.includes(response))
+			if(!rank) {
+				return message.channel.send(`${response} não é um rank configurado`)
+			}
+		} 
 
 		let description = ''
-		for(const ranks in data.rank) {
-			description += `Rank ${ranks}: ${data.rank[ranks].name} ${data.config.description}${data.rank[ranks].value}\n` 
+		for(const rankList in rank.rank) {
+			description += `Rank ${rankList}: ${rank.rank[rankList].name} ${rank.config.description}${rank.rank[rankList].value}\n` 
 		}
 		
 		const embed = new Discord.MessageEmbed()
 			.setColor('#00ff00')
-			.setTitle(`Rank de ${data.config.nameDesciption}`)
+			.setTitle(`Rank de ${rank.config.nameDesciption}`)
 			.setDescription(description)
 			.setTimestamp()
 			.setFooter('[Bot feito por Censoretti]', 'https://cdn.discordapp.com/attachments/613477001071951915/703825043066585168/Screenshot_5.png')
 
-		message.channel.send(embed)
+		return message.channel.send(embed)
 	},
 };
